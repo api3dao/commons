4bbdd1b2f81ef67edffff09db0b2889fb99f9d1e
Emanuel Tesař <e.tesarr@gmail.com>
2023-10-09 14:24:35 +0200

    Improve tests, remove unnecessary code

--- src/processing/processing.test.ts
+++ src/processing/processing.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable jest/prefer-strict-equal */ // Because the errors are thrown from the "vm" module (different context), they are not strictly equal.
-import * as fixtures from '../../test/fixtures';
+import { createEndpoint } from '../../test/fixtures';
 
 import {
   addReservedParameters,
@@ -8,27 +8,25 @@ import {
   removeReservedParameters,
 } from './processing';
 
-// TODO: Remove the not needed fixtures for processing
-// TODO: Add a bit more tests
 describe(preProcessApiCallParameters.name, () => {
   it('valid processing code', async () => {
-    const config = fixtures.buildConfig();
-    const preProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        value: 'const output = {...input, from: "ETH"};',
-        timeoutMs: 5000,
-      },
-      {
-        environment: 'Node' as const,
-        value: 'const output = {...input, newProp: "airnode"};',
-        timeoutMs: 5000,
-      },
-    ];
-    config.ois[0]!.endpoints[0] = { ...config.ois[0]!.endpoints[0]!, preProcessingSpecifications };
+    const endpoint = createEndpoint({
+      preProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: 'const output = {...input, from: "ETH"};',
+          timeoutMs: 5000,
+        },
+        {
+          environment: 'Node',
+          value: 'const output = {...input, newProp: "airnode"};',
+          timeoutMs: 5000,
+        },
+      ],
+    });
     const parameters = { _type: 'int256', _path: 'price' };
 
-    const result = await preProcessApiCallParameters(config.ois[0]!.endpoints[0]!, parameters);
+    const result = await preProcessApiCallParameters(endpoint, parameters);
 
     expect(result).toEqual({
       _path: 'price',
@@ -39,43 +37,43 @@ describe(preProcessApiCallParameters.name, () => {
   });
 
   it('invalid processing code', async () => {
-    const config = fixtures.buildConfig();
-    const preProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        value: 'something invalid; const output = {...input, from: `ETH`};',
-        timeoutMs: 5000,
-      },
-      {
-        environment: 'Node' as const,
-        value: 'const output = {...input, newProp: "airnode"};',
-        timeoutMs: 5000,
-      },
-    ];
-    config.ois[0]!.endpoints[0] = { ...config.ois[0]!.endpoints[0]!, preProcessingSpecifications };
+    const endpoint = createEndpoint({
+      preProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: 'something invalid; const output = {...input, from: `ETH`};',
+          timeoutMs: 5000,
+        },
+        {
+          environment: 'Node',
+          value: 'const output = {...input, newProp: "airnode"};',
+          timeoutMs: 5000,
+        },
+      ],
+    });
     const parameters = { _type: 'int256', _path: 'price', from: 'TBD' };
 
-    const throwingFunc = async () => preProcessApiCallParameters(config.ois[0]!.endpoints[0]!, parameters);
+    const throwingFunc = async () => preProcessApiCallParameters(endpoint, parameters);
 
     await expect(throwingFunc).rejects.toEqual(new Error('SyntaxError: Unexpected identifier'));
   });
 
-  it('demonstrates access to endPointParameters, but reserved parameters are inaccessible', async () => {
-    const config = fixtures.buildConfig();
-    const preProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        // pretend the user is trying to 1) override _path and 2) set a new parameter based on
-        // the presence of the reserved parameter _type (which is inaccessible)
-        value:
-          'const output = {...input, from: "ETH", _path: "price.newpath", myVal: input._type ? "123" : "456", newTo: endpointParameters.to };',
-        timeoutMs: 5000,
-      },
-    ];
-    config.ois[0]!.endpoints[0] = { ...config.ois[0]!.endpoints[0]!, preProcessingSpecifications };
+  it('demonstrates access to endpointParameters, but reserved parameters are inaccessible', async () => {
     const parameters = { _type: 'int256', _path: 'price', to: 'USD' };
+    const endpoint = createEndpoint({
+      preProcessingSpecifications: [
+        {
+          environment: 'Node',
+          // pretend the user is trying to 1) override _path and 2) set a new parameter based on
+          // the presence of the reserved parameter _type (which is inaccessible)
+          value:
+            'const output = {...input, from: "ETH", _path: "price.newpath", myVal: input._type ? "123" : "456", newTo: endpointParameters.to };',
+          timeoutMs: 5000,
+        },
+      ],
+    });
 
-    const result = await preProcessApiCallParameters(config.ois[0]!.endpoints[0]!, parameters);
+    const result = await preProcessApiCallParameters(endpoint, parameters);
 
     expect(result).toEqual({
       _path: 'price', // is not overridden
@@ -86,44 +84,93 @@ describe(preProcessApiCallParameters.name, () => {
       newTo: 'USD', // demonstrates access to endpointParameters
     });
   });
+
+  it('uses native modules for processing', async () => {
+    const endpoint = createEndpoint({
+      preProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: `
+          const randomValue = crypto.randomBytes(4).toString('hex');
+          const output = {...input, randomValue};
+        `,
+          timeoutMs: 5000,
+        },
+      ],
+    });
+    const parameters = { _type: 'int256', _path: 'price' };
+
+    const result = await preProcessApiCallParameters(endpoint, parameters);
+
+    // Check that the result contains the original parameters and a valid 8-character hex random value.
+    expect(result).toMatchObject({
+      _path: 'price',
+      _type: 'int256',
+    });
+    expect(result.randomValue).toHaveLength(8);
+    expect(/^[\da-f]{8}$/i.test(result.randomValue)).toBe(true);
+  });
+
+  it('throws error due to processing timeout', async () => {
+    const endpoint = createEndpoint({
+      preProcessingSpecifications: [
+        {
+          environment: 'Node async',
+          value: `
+          const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+          delay(5000);
+          const output = {...input, from: 'ETH'};
+        `,
+          timeoutMs: 100, // This timeout is shorter than the delay in the processing code.
+        },
+      ],
+    });
+    const parameters = { _type: 'int256', _path: 'price' };
+
+    const throwingFunc = async () => preProcessApiCallParameters(endpoint, parameters);
+
+    await expect(throwingFunc).rejects.toThrow('Timeout exceeded');
+  });
 });
 
 describe(postProcessApiCallResponse.name, () => {
   it('processes valid code', async () => {
-    const config = fixtures.buildConfig();
-    const postProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        value: 'const output = parseInt(input.price)*2;',
-        timeoutMs: 5000,
-      },
-      {
-        environment: 'Node' as const,
-        value: 'const output = parseInt(input)*2;',
-        timeoutMs: 5000,
-      },
-    ];
-    const endpoint = { ...config.ois[0]!.endpoints[0]!, postProcessingSpecifications };
     const parameters = { _type: 'int256', _path: 'price' };
+    const endpoint = createEndpoint({
+      postProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: 'const output = parseInt(input.price)*2;',
+          timeoutMs: 5000,
+        },
+        {
+          environment: 'Node',
+          value: 'const output = parseInt(input)*2;',
+          timeoutMs: 5000,
+        },
+      ],
+    });
 
     const result = await postProcessApiCallResponse({ price: 1000 }, endpoint, parameters);
 
     expect(result).toBe(4000);
   });
 
-  it('demonstrates access to endPointParameters, but reserved parameters are inaccessible', async () => {
-    const config = fixtures.buildConfig();
-    const postProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        value:
-          'const reservedMultiplier = endpointParameters._times ? 1 : 2; const output = parseInt(input.price) * endpointParameters.myMultiplier * reservedMultiplier;',
-        timeoutMs: 5000,
-      },
-    ];
-    const endpoint = { ...config.ois[0]!.endpoints[0]!, postProcessingSpecifications };
+  it('demonstrates access to endpointParameters, but reserved parameters are inaccessible', async () => {
     const myMultiplier = 10;
     const parameters = { _type: 'int256', _path: 'price', myMultiplier };
+    const endpoint = createEndpoint({
+      postProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: `
+            const reservedMultiplier = endpointParameters._times ? 1 : 2;
+            const output = parseInt(input.price) * endpointParameters.myMultiplier * reservedMultiplier
+          `,
+          timeoutMs: 5000,
+        },
+      ],
+    });
 
     const price = 1000;
     const result = await postProcessApiCallResponse({ price }, endpoint, parameters);
@@ -134,21 +181,24 @@ describe(postProcessApiCallResponse.name, () => {
   });
 
   it('throws on invalid code', async () => {
-    const config = fixtures.buildConfig();
-    const postProcessingSpecifications = [
-      {
-        environment: 'Node' as const,
-        value: 'const output = parseInt(input.price)*1000;',
-        timeoutMs: 5000,
-      },
-      {
-        environment: 'Node' as const,
-        value: 'Something Unexpected; const output = parseInt(input)*2;',
-        timeoutMs: 5000,
-      },
-    ];
-    const endpoint = { ...config.ois[0]!.endpoints[0]!, postProcessingSpecifications };
     const parameters = { _type: 'int256', _path: 'price' };
+    const endpoint = createEndpoint({
+      postProcessingSpecifications: [
+        {
+          environment: 'Node',
+          value: 'const output = parseInt(input.price)*1000;',
+          timeoutMs: 5000,
+        },
+        {
+          environment: 'Node',
+          value: `
+            Something Unexpected;
+            const output = parseInt(input)*2;
+          `,
+          timeoutMs: 5000,
+        },
+      ],
+    });
 
     const throwingFunc = async () => postProcessApiCallResponse({ price: 1000 }, endpoint, parameters);
 
--- test/fixtures.ts
+++ test/fixtures.ts
@@ -1,149 +1,49 @@
-import type { OIS } from '@api3/ois';
+import type { Endpoint } from '@api3/ois';
 
-import type { ApiCredentials, Config, RegularAggregatedApiCall } from '../src/processing/schema';
-
-export function buildAggregatedRegularApiCall(params?: Partial<RegularAggregatedApiCall>): RegularAggregatedApiCall {
+export const createEndpoint = (overrides: Partial<Endpoint>): Endpoint => {
   return {
-    airnodeAddress: '0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace',
-    cacheResponses: false,
-    chainId: '31337',
-    endpointId: '0x13dea3311fe0d6b84f4daeab831befbc49e19e6494c41e9e065a09c3c68f43b6',
-    endpointName: 'convertToUSD',
-    id: '0xf40127616f09d41b20891bcfd326957a0e3d5a5ecf659cff4d8106c04b024374',
-    oisTitle: 'Currency Converter API',
-    parameters: { from: 'ETH' },
-    requestType: 'full',
-    requesterAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-    sponsorAddress: '0x2479808b1216E998309A727df8A0A98A1130A162',
-    sponsorWalletAddress: '0x1C1CEEF1a887eDeAB20219889971e1fd4645b55D',
-    templateId: null,
-
-    encodedParameters:
-      '0x317373737373000000000000000000000000000000000000000000000000000066726f6d000000000000000000000000000000000000000000000000000000004554480000000000000000000000000000000000000000000000000000000000746f00000000000000000000000000000000000000000000000000000000000055534400000000000000000000000000000000000000000000000000000000005f74797065000000000000000000000000000000000000000000000000000000696e7432353600000000000000000000000000000000000000000000000000005f70617468000000000000000000000000000000000000000000000000000000726573756c7400000000000000000000000000000000000000000000000000005f74696d657300000000000000000000000000000000000000000000000000003130303030300000000000000000000000000000000000000000000000000000',
-    fulfillAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
-    fulfillFunctionId: '0x7c1de7e1',
-    metadata: {
-      address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
-      blockNumber: 11,
-      currentBlock: 12,
-      minConfirmations: 0,
-      transactionHash: '0x40b93a1e81c7162460af066be96266ff692515a2f6b54bd622aa9f82ee00670f',
-      logIndex: 0,
+    name: 'convertToUSD',
+    operation: {
+      method: 'get',
+      path: '/convert',
     },
-    requestCount: '1',
-    ...params,
-  };
-}
-
-export function buildApiCredentials(overrides?: Partial<ApiCredentials>): ApiCredentials {
-  return {
-    securitySchemeName: 'myApiSecurityScheme',
-    securitySchemeValue: 'supersecret',
-    oisTitle: 'Currency Converter API',
-    ...overrides,
-  };
-}
-
-export function buildOIS(ois?: Partial<OIS>): OIS {
-  return {
-    oisFormat: '2.2.1',
-    version: '1.2.3',
-    title: 'Currency Converter API',
-    apiSpecifications: {
-      servers: [
-        {
-          url: 'http://localhost:5000',
-        },
-      ],
-      paths: {
-        '/convert': {
-          get: {
-            parameters: [
-              {
-                in: 'query',
-                name: 'from',
-              },
-              {
-                in: 'query',
-                name: 'to',
-              },
-              {
-                in: 'query',
-                name: 'amount',
-              },
-            ],
-          },
-        },
-      },
-      components: {
-        securitySchemes: {
-          myApiSecurityScheme: {
-            in: 'query',
-            type: 'apiKey',
-            name: 'access_key',
-          },
-        },
-      },
-      security: {
-        myApiSecurityScheme: [],
-      },
-    },
-    endpoints: [
+    fixedOperationParameters: [
       {
-        name: 'convertToUSD',
-        operation: {
-          method: 'get',
-          path: '/convert',
+        operationParameter: {
+          in: 'query',
+          name: 'to',
         },
-        fixedOperationParameters: [
-          {
-            operationParameter: {
-              in: 'query',
-              name: 'to',
-            },
-            value: 'USD',
-          },
-        ],
-        reservedParameters: [
-          { name: '_type' },
-          { name: '_path' },
-          {
-            name: '_times',
-            default: '100000',
-          },
-          { name: '_gasPrice' },
-          { name: '_minConfirmations' },
-        ],
-        parameters: [
-          {
-            name: 'from',
-            default: 'EUR',
-            operationParameter: {
-              in: 'query',
-              name: 'from',
-            },
-          },
-          {
-            name: 'amount',
-            default: '1',
-            operationParameter: {
-              name: 'amount',
-              in: 'query',
-            },
-          },
-        ],
+        value: 'USD',
       },
     ],
-    ...ois,
-  };
-}
-
-export function buildConfig(overrides?: Partial<Config>): Config {
-  const rawConfig: Config = {
-    ois: [buildOIS()],
-    apiCredentials: [buildApiCredentials()],
+    reservedParameters: [
+      { name: '_type' },
+      { name: '_path' },
+      {
+        name: '_times',
+        default: '100000',
+      },
+      { name: '_gasPrice' },
+      { name: '_minConfirmations' },
+    ],
+    parameters: [
+      {
+        name: 'from',
+        default: 'EUR',
+        operationParameter: {
+          in: 'query',
+          name: 'from',
+        },
+      },
+      {
+        name: 'amount',
+        default: '1',
+        operationParameter: {
+          name: 'amount',
+          in: 'query',
+        },
+      },
+    ],
     ...overrides,
   };
-
-  return rawConfig;
-}
+};
