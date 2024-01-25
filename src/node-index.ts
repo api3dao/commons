613152c1c02101e47ad1f7f103bbb6ac567f258d
Emanuel Tesař <e.tesarr@gmail.com>
2024-01-25 12:01:27 +0100

    Create separate entry point for FE and BE

--- src/node-index.ts
+++ src/node-index.ts
@@ -1,4 +1,4 @@
 // NOTE: Not exporting ESLint rules because they need to be imported in a special way inside .eslintrc.js config.
+export * from './universal-index';
 export * from './logger';
 export * from './processing';
-export * from './blockchain-utilities';
---
+++ src/universal-index.ts
@@ -0,0 +1,2 @@
+// NOTE: Only export modules which work in both Node.js and browser environments.
+export * from './blockchain-utilities';
