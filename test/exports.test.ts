a90528947eea84f1253726267d162b97dd68605e
Derek Croote <dcroote@users.noreply.github.com>
2024-06-24 22:57:41 -0700

    Replace eslint plugin with @api3/eslint-plugin-commons

--- test/exports.test.ts
+++ test/exports.test.ts
@@ -1,8 +1,6 @@
 import { readFileSync, readdirSync } from 'node:fs';
 import { join } from 'node:path';
 
-import difference from 'lodash/difference';
-
 const getExports = (filename: string) => {
   return readFileSync(join(__dirname, '../src', filename), 'utf8')
     .split('\n')
@@ -19,9 +17,6 @@ test('all modules are exported', () => {
   // Node imports all universal exports, so it needs to be excluded.
   const nodeExports = getExports('node-index.ts').filter((exp) => exp !== 'universal-index');
   const allExports = [...nodeExports, ...universalExports].sort();
-  const intentionallyUnexported = [
-    'eslint', // ESLint rules are meant to be used directly in .eslintrc.js and are not meant to be imported.
-  ];
 
-  expect(difference(srcModules, intentionallyUnexported)).toStrictEqual(allExports);
+  expect(srcModules).toStrictEqual(allExports);
 });
