d1eebfaa80d0e9f77645fabb501f035e12f91cd4
Derek Croote <dcroote@users.noreply.github.com>
2024-06-25 21:59:24 -0700

    Remove two eslint-disable lines that are no longer necessary

--- src/logger/index.test.ts
+++ src/logger/index.test.ts
@@ -101,7 +101,7 @@ test('returns rejected promise if the async callback function rejects', async ()
   const { logger } = createTestLogger();
 
   await expect(async () =>
-    // eslint-disable-next-line @typescript-eslint/require-await
+     
     logger.runWithContext({}, async () => {
       throw new Error('some-error');
     })
